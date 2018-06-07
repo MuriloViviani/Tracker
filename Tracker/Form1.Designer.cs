163bc6dc8c065a615010c719d48f1739197a8728
murilo viviani <muriloviviani@hotmail.com>
2018-06-07 08:49:44 -0300

    commit

--- Tracker/Form1.Designer.cs
+++ Tracker/Form1.Designer.cs
@@ -29,12 +29,252 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lsvHistoric = new System.Windows.Forms.ListView();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SecondsTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTaskName = new System.Windows.Forms.TextBox();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblCurrentTask = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Lime;
+            this.btnStart.Location = new System.Drawing.Point(560, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(228, 75);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Location = new System.Drawing.Point(560, 93);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(228, 75);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lsvHistoric
+            // 
+            this.lsvHistoric.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lsvHistoric.FullRowSelect = true;
+            this.lsvHistoric.GridLines = true;
+            this.lsvHistoric.Location = new System.Drawing.Point(12, 174);
+            this.lsvHistoric.Name = "lsvHistoric";
+            this.lsvHistoric.Size = new System.Drawing.Size(776, 241);
+            this.lsvHistoric.TabIndex = 2;
+            this.lsvHistoric.UseCompatibleStateImageBehavior = false;
+            this.lsvHistoric.View = System.Windows.Forms.View.Details;
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.Location = new System.Drawing.Point(44, 102);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(82, 55);
+            this.lblHours.TabIndex = 3;
+            this.lblHours.Text = "00";
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinutes.Location = new System.Drawing.Point(132, 102);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(82, 55);
+            this.lblMinutes.TabIndex = 4;
+            this.lblMinutes.Text = "00";
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeconds.Location = new System.Drawing.Point(220, 102);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(82, 55);
+            this.lblSeconds.TabIndex = 5;
+            this.lblSeconds.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(115, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 40);
+            this.label4.TabIndex = 6;
+            this.label4.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(204, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 40);
+            this.label5.TabIndex = 7;
+            this.label5.Text = ":";
+            // 
+            // SecondsTimer
+            // 
+            this.SecondsTimer.Interval = 1000;
+            this.SecondsTimer.Tick += new System.EventHandler(this.SecondsTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Hours";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Minutes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(217, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Seconds";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Task Name";
+            // 
+            // txtTaskName
+            // 
+            this.txtTaskName.Location = new System.Drawing.Point(47, 26);
+            this.txtTaskName.Name = "txtTaskName";
+            this.txtTaskName.Size = new System.Drawing.Size(490, 20);
+            this.txtTaskName.TabIndex = 12;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Task Name";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Start";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Stop";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Spent Time";
+            this.columnHeader4.Width = 120;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(50, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Current Task -";
+            // 
+            // lblCurrentTask
+            // 
+            this.lblCurrentTask.AutoSize = true;
+            this.lblCurrentTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentTask.Location = new System.Drawing.Point(123, 54);
+            this.lblCurrentTask.Name = "lblCurrentTask";
+            this.lblCurrentTask.Size = new System.Drawing.Size(101, 13);
+            this.lblCurrentTask.TabIndex = 14;
+            this.lblCurrentTask.Text = "No Task running";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 425);
+            this.Controls.Add(this.lblCurrentTask);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTaskName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.lblMinutes);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.lsvHistoric);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Tasker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListView lsvHistoric;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer SecondsTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTaskName;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblCurrentTask;
     }
 }
 
--- Tracker/Form1.cs
+++ Tracker/Form1.cs
@@ -1,20 +1,130 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Tracker
 {
     public partial class Form1 : Form
     {
+        private int seconds = 0, minutes = 0, hours = 0;
+        private bool running = false;
+
+        // Task Management variables
+        string[] task = new string[4];
+
         public Form1()
         {
             InitializeComponent();
+
+            btnStop.Enabled = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                if (txtTaskName.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("You Need to inform the task name first");
+                    txtTaskName.Focus();
+
+                    return;
+                }
+                else
+                    SaveTask();
+            }
+            else
+            {
+                if (txtTaskName.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("You Need to inform the task name first");
+                    txtTaskName.Focus();
+
+                    return;
+                }
+                else
+                {
+                    running = true;
+                    btnStart.Text = "New Task";
+
+                    btnStop.Enabled = true;
+                    SecondsTimer.Enabled = true;
+                }
+            }
+
+            // Get new task information
+            task[0] = txtTaskName.Text;
+            task[1] = DateTime.Now.ToString("HH:mm:ss");
+
+            Reset();
+
+            lblCurrentTask.Text = task[0];
+        }
+
+        private void SecondsTimer_Tick(object sender, EventArgs e)
+        {
+            if (seconds >= 59)
+            {
+                seconds = 0;
+                if (minutes >= 59)
+                {
+                    minutes = 0;
+                    hours++;
+                }
+                else { minutes++; }
+            }
+            else { seconds++; }
+
+            UpdateTimerLabels();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            SaveTask();
+
+            SecondsTimer.Enabled = false;
+
+            Reset();
+
+            btnStart.Text = "Start";
+            btnStop.Enabled = false;
+            running = false;
+        }
+
+
+        private void SaveTask()
+        {
+            var initialTime = DateTime.Parse(task[1]);
+            var finalTime = DateTime.Now;
+
+            // Get the final time
+            task[2] = finalTime.ToString("HH:mm:ss");
+
+            task[3] = finalTime.Subtract(initialTime).ToString();
+
+            // Save current task in the List View
+            var listViewItem = new ListViewItem(task);
+            lsvHistoric.Items.Add(listViewItem);
+        }
+
+        private void UpdateTimerLabels()
+        {
+            lblSeconds.Text = seconds.ToString().PadLeft(2);
+            lblMinutes.Text = minutes.ToString().PadLeft(2);
+            lblHours.Text = hours.ToString().PadLeft(2);
+        }
+
+        private void Reset()
+        {
+            seconds = 0;
+            minutes = 0;
+            hours = 0;
+
+            txtTaskName.Text = "";
+            txtTaskName.Focus();
+
+            lblCurrentTask.Text = "No Task running";
+
+            UpdateTimerLabels();
         }
     }
 }
